d2e53ec18b7ea7ae9f20621b7856f77e5c910043
SSRa1n <phukawinzagamerz@gmail.com>
2026-08-07 21:55:22 +0700

    implement preset maps prototype

--- src/pages/presetmap_page.tsx
+++ src/pages/presetmap_page.tsx
@@ -1,3 +1,11 @@
+import { PRESETS } from "../presets/map_presets";
+import RenderMaze from "../lib/render_maze";
+
 export default function PresetmapPage() {
-    return <h1><b>PRESET MAP UNDER CONSTRUCTION</b></h1>
+    return (
+        <div className="presetmap-page">
+            <h1>Preset Maps</h1>
+            <RenderMaze maze={PRESETS[0]} renderCost={true} />
+        </div>
+    );
 }

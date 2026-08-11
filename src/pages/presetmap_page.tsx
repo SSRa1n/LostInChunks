72ea386d8daf0305de7d6e1b5947b4f6c56efda4
SSRa1n <phukawinzagamerz@gmail.com>
2026-08-11 09:14:57 +0700

    added preset map

--- src/pages/presetmap_page.tsx
+++ src/pages/presetmap_page.tsx
@@ -1,11 +1,15 @@
 import { PRESETS } from "../presets/map_presets";
 import RenderMaze from "../lib/render_maze";
+import SingularView from "../components/singular_view/singular_view";   
 
 export default function PresetmapPage() {
     return (
         <div className="presetmap-page">
             <h1>Preset Maps</h1>
-            <RenderMaze maze={PRESETS[0]} renderCost={true} />
+            {/* <RenderMaze maze={PRESETS[0]} renderCost={true} /> */}
+            <div className="presetmap-grid">
+                <SingularView maze={PRESETS[0]} renderCost={true} />
+            </div>
         </div>
     );
 }

310789ac8bf056cf28c35ac045b44646a177c9a8
Kurumizawaa <1847123@gmail.com>
2026-09-05 01:56:35 +0700

    add option for page specific background

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
-import './App.css'
+import './App.css';
+
 import ProblemPage from './pages/problem_page.tsx/problem_page';
-import ComparisonPage from './pages/comparison_page/comparison_page'
-import PresetmapPage from './pages/presetmap_page/presetmap_page'
-import AboutPage from './pages/about_page/about_page'
-import PolicyPage from './pages/policy_page/policy_page'
-import PageFooter from './components/page_footer/footer'
+import ComparisonPage from './pages/comparison_page/comparison_page';
+import PresetmapPage from './pages/presetmap_page/presetmap_page';
+import AboutPage from './pages/about_page/about_page';
+import PolicyPage from './pages/policy_page/policy_page';
+
+import PageFooter from './components/page_footer/footer';
 import Navbar from './components/navbar/navbar';
 
-function App() {
+import stoneBackground from "/blocks/stone.png";
+import lightBlueConcreteBackground from "/blocks/light_blue_concrete.png"
+import endPortalBackground from "/blocks/end_portal.png";
+
+const pageBackgrounds: Record<string, string> = {
+  "/": stoneBackground,
+  "/about": lightBlueConcreteBackground,
+};
+
+function Layout() {
+  const location = useLocation();
+
+  const background =
+    pageBackgrounds[location.pathname] ?? endPortalBackground;
 
   return (
-    <Router>
+    <div
+      className="layout"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
       <Navbar />
+
       <Routes>
         <Route path="/" element={<ProblemPage />} />
         <Route path="/comparison" element={<ComparisonPage />} />
@@ -21,9 +42,18 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/policy" element={<PolicyPage />} />
       </Routes>
+
       <PageFooter />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout />
     </Router>
-  )
+  );
 }
 
 export default App
